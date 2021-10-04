19ebf4519fd3b3fb0d45c2b14e4a7d0f9f4dfe1f
Adriano Pimenta <apimenta.usp@gmail.com>
2021-10-04 12:43:51 -0300

    Inserindo label Bem vindo

--- TestandoGitHub/Form1.Designer.cs
+++ TestandoGitHub/Form1.Designer.cs
@@ -24,13 +24,35 @@ namespace TestandoGitHub {
         /// o conteúdo deste método com o editor de código.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(275, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(229, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bem vindo!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
